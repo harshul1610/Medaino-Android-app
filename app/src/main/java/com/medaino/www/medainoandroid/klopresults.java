fae3abe5b8d2a3ea9b7d7ebb6e6ef0a8f5027650
harshul1610 <harshulrobo@gmail.com>
2015-07-30 18:17:19 +0530

    added bluetooth interface at klopresults

--- app/src/main/java/com/medaino/www/medainoandroid/klopresults.java
+++ app/src/main/java/com/medaino/www/medainoandroid/klopresults.java
@@ -24,22 +24,41 @@ import java.util.logging.LogRecord;
 
 
 public class klopresults extends Activity{
-
+    BluetoothSocket mmSocket;
+    InputStream mmInStream=null;
+    OutputStream mmOutStream=null;
+    String data=new String(" ");
+    String red=new String();
+    String ir=new String();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_klopresults);
+        BluetoothDevice tmpl=(BluetoothDevice)getIntent().getExtras().getParcelable("key2");
 
+        ConnectThread con_bl=new ConnectThread(tmpl);
+        con_bl.start();
 
     } //end of oncreate method
 
 
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        try {
+            mmInStream.close();
+            mmOutStream.close();
+            mmSocket.close();
+        }
+        catch (IOException e)
+        {
+
+        }
 
 
-
-
-
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,6 +81,148 @@ public class klopresults extends Activity{
 
         return super.onOptionsItemSelected(item);
     }
+
+    class ConnectThread extends Thread
+    {
+        BluetoothDevice mmDevice;
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+        public ConnectThread(BluetoothDevice device)
+        {
+
+            mmDevice = device;
+
+
+            // Get a BluetoothSocket to connect with the given BluetoothDevice
+            try
+            {
+                mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+            }
+            catch (IOException e)
+            {
+
+            }
+
+        }
+
+        public void run()
+        {
+
+            try
+            {
+                mmSocket.connect();
+                Log.d("balle1", "connectivity done") ;
+            }
+            catch (IOException connectException)
+            {
+                Log.d("balle2","connectivity cant be done") ;
+            }
+
+            ConnectedThread manageConnectedSocket=new ConnectedThread(mmSocket);
+            manageConnectedSocket.start();
+
+        }
+    }
+
+
+
+
+
+
+
+    class ConnectedThread extends Thread
+    {
+        int readBufferPosition=0;
+
+        private void closeConnection()
+        {
+            if (mmInStream != null)
+            {
+                try {mmInStream.close();} catch (Exception e) {}
+                mmInStream = null;
+            }
+
+            if (mmOutStream != null)
+            {
+                try {mmOutStream.close();} catch (Exception e) {}
+                mmOutStream = null;
+            }
+
+            if (mmSocket != null)
+            {
+                try {mmSocket.close();} catch (Exception e) {}
+                mmSocket = null;
+            }
+
+        }
+
+        public ConnectedThread(BluetoothSocket socket)
+        {
+            mmSocket = socket;
+
+            try
+            {
+                mmInStream = socket.getInputStream();
+                mmOutStream = socket.getOutputStream();
+
+            }
+            catch (IOException e)
+            {
+                Log.d("yyyy","cant take");
+            }
+        }
+
+        public void run()
+        {
+
+            byte[] readBuffer=new byte[1024];
+            while (true)
+            {
+                try
+                {
+                    int bytesAvailable = mmInStream.available();
+                    if(bytesAvailable > 0)
+                    {
+                        byte[] packetBytes = new byte[bytesAvailable];
+                        mmInStream.read(packetBytes);
+                        for(int i=0;i<bytesAvailable;i++)
+                        {
+                            byte b = packetBytes[i];
+                            if (b==',')
+                            {
+                                byte[] encodedBytes = new byte[readBufferPosition];
+                                System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                data = new String(encodedBytes, "US-ASCII");
+                                String[] parts = data.split(" ");
+                                red=parts[0];
+                                ir=parts[1];
+                                readBufferPosition = 0;
+
+
+                            }
+                            else {
+                                readBuffer[readBufferPosition++] = b;
+                            }
+                        }
+
+                    }
+                    Log.d("xxxx", "storing and transmitting data");
+
+                }
+                catch (IOException e) {
+                    Log.d("iiii","can't store");
+                    break;
+                }
+                //    //within while   */
+
+
+            }    //end of while
+
+        }          //end of run
+
+
+    }
+
 }
 
 
